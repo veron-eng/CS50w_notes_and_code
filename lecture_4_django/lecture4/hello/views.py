f67661210467677578a1bff049944038cb4874c2
veron-eng <veronsundaram13@gmail.com>
2024-02-19 11:53:29 +0000

    finished lecture 3 and learnt forms in django

---
+++ lecture_4_django/lecture4/hello/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# def index(request):
+#     return HttpResponse("Hello,world! ")
+
+def index(request):
+    return render(request, "hello/index.html")
+
+def veron(request):
+    return HttpResponse("Hello,Veron!")
+
+# def greet(request,name):
+#     return HttpResponse(f"Hello,{name.capitalize()}!")
+
+
+def greet(request,name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
